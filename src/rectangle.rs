5816ccfb98a62a10249bb60c50a1150bb7fa4a0c
Joseph <josephmshimel@gmail.com>
2024-12-30 16:56:14 -0700

    add rectangle contains 2d point

--- src/rectangle.rs
+++ src/rectangle.rs
@@ -14,4 +14,11 @@ impl Rectangle {
             size: nalgebra_glm::vec2(w, h),
         }
     }
+
+    pub fn contains_point(&self, p: &nalgebra_glm::Vec2) -> bool {
+        self.pos.x <= p.x
+            && self.pos.y <= p.y
+            && self.pos.x + self.size.x >= p.x
+            && self.pos.y + self.size.y >= p.y
+    }
 }

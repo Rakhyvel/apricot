ce584e31cc055a8bb8606998ad46badad41f84e1
Joseph <josephmshimel@gmail.com>
2024-12-31 19:07:20 -0700

    debug

--- src/render_core.rs
+++ src/render_core.rs
@@ -438,7 +438,9 @@ impl RenderContext {
             );
             gl::BindVertexArray(line_path.vao);
             gl::DrawArrays(gl::LINE_LOOP, 0, line_path.num_vertices);
+
             gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+            gl::BindVertexArray(0);
         }
     }
 }

0786c976c9fe907ce1273f1858d70f7ede5695c0
Joseph <josephmshimel@gmail.com>
2026-03-22 19:49:57 -0600

    add ray, fix hecs version

--- src/ray.rs
+++ src/ray.rs
@@ -5,3 +5,9 @@ pub struct Ray {
     pub origin: nalgebra_glm::Vec3,
     pub dir: nalgebra_glm::Vec3,
 }
+
+impl Ray {
+    pub fn point_at_distance(&self, distance: f32) -> nalgebra_glm::Vec3 {
+        self.origin + self.dir.normalize() * distance
+    }
+}

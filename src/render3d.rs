edffffe4a2c4e1bc3e49e8b1061f3be76a824f0c
Joseph <josephmshimel@gmail.com>
2026-03-22 19:30:18 -0600

    fix query

--- src/render3d.rs
+++ src/render3d.rs
@@ -138,7 +138,7 @@ impl RenderContext {
             gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
         }
         let (view_matrix, proj_matrix) = self.camera.borrow().view_proj_matrices();
-        for (_entity, line_path) in world.query::<&LinePathComponent>().iter() {
+        for line_path in world.query::<&LinePathComponent>().iter() {
             self.draw_line_path(line_path, view_matrix, proj_matrix)
         }
     }

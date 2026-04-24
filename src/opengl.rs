d6d150133529000edb6a87ba1704f570351fb3d3
Joseph <josephmshimel@gmail.com>
2026-04-24 01:26:02 -0600

    Revert "fix vertex based alpha"
    This reverts commit 39e022efe3f8671211a9fd556856817f6b3a0e78.

--- src/opengl.rs
+++ src/opengl.rs
@@ -295,13 +295,6 @@ impl Vao {
         print_any_errors();
     }
 
-    pub fn bind_vao(&self) {
-        unsafe {
-            gl::BindVertexArray(self.id);
-        }
-        print_any_errors();
-    }
-
     fn setup(&self, loc: u32) {
         unsafe {
             gl::VertexAttribPointer(
